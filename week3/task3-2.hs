1ce0a4bbfeba0128785567627265de80fa6b86b2
Toni Immonen <toni.immonen0@gmail.com>
2019-09-15 00:05:49 +0300

    Task 3.2 first iter complete

---
+++ week3/task3-2.hs
@@ -0,0 +1,17 @@
+import Data.List
+
+f :: String -> String -> Float
+f [] [] = 0
+f xs ys = 
+    let xCharsNotInY = fromIntegral . length . snd $ partition (`elem` ys) xs
+        yCharsNotInX = fromIntegral . length . snd $ partition (`elem` xs) ys
+        lengthSum    = fromIntegral $ length xs + length ys
+    in  (xCharsNotInY + yCharsNotInX) / lengthSum
+
+g :: String -> String -> Float
+g [] [] = 0
+g xs ys =
+    let xCharsNotNums = fromIntegral . length . snd $ partition (`elem` ['0'..'9']) xs
+        yCharsNotNums = fromIntegral . length . snd $ partition (`elem` ['0'..'9']) ys
+        lengthSum     = fromIntegral $ length xs + length ys
+    in  (xCharsNotNums + yCharsNotNums) / lengthSum
