6afd9dbf0965687401fec017cf5d579bdd640609
Toni Immonen <toni.immonen0@gmail.com>
2019-09-29 18:42:49 +0300

    Task 5.1 complete

--- week5/task5-1.hs
+++ week5/task5-1.hs
@@ -1,18 +1,3 @@
--- Task 4.3
--- Creates a telephone number if the input is correct.
-readPhone :: String -> String -> String -> Phone
-readPhone pt cc pn = 
-    Phone { phoneType = read pt
-          , countryCode = toCountryCode . read $ parseCountryCode cc
-          , phoneNo = toPhoneNo $ read pn
-          }
-    where parseCountryCode cc
-            | length cc < 1 = error "Invalid country code."
-            | head cc == '+' = (parseCountryCode $ tail cc)
-            | take 2 cc == "00" = (parseCountryCode $ drop 2 cc)
-            | cc `elem` listOfSomeCountryCodes = cc
-            | otherwise = error "Invalid country code."
-
 -- Predefined list of some country codes that are valid
 listOfSomeCountryCodes = ["1", "242", "236", "243", "420", "358", "996", "598", "666"]
 
@@ -27,7 +12,6 @@ newtype PhoneNo = PhoneNo Integer
 data PhoneType = WorkLandline | PrivateMobile | WorkMobile | Other
     deriving (Show, Eq, Read)
 
-
 data Phone = Phone { phoneType   :: Maybe PhoneType
                    , countryCode :: Maybe CountryCode
                    , phoneNo     :: PhoneNo
@@ -41,20 +25,30 @@ instance Show PhoneNo where
     show (PhoneNo pn) = show pn
 
 instance Show Phone where
-    show (Phone pt cc pn) = show cc ++ " " ++ show pn ++ " (" ++ show pt ++ ")"
+    show (Phone pt cc pn) = maybe "" (foldr (:) " " . show) cc
+                            ++ show pn
+                            ++ maybe "" ((++) " (" . foldr (:) ")" . show) pt
 
--- Functions to create contry code and phone number data types
+
 toCountryCode :: Integer -> Maybe CountryCode
 toCountryCode cc
-    | cc >= 0 = Just (CountryCode cc)
-    | otherwise = error "Country code must be a positive integer."
+        | cc >= 0 = Just $ CountryCode cc
+        | otherwise = Nothing
 
 toPhoneNo :: Integer -> PhoneNo
 toPhoneNo pn
-    | pn >= 0 = PhoneNo pn
-    | otherwise = error "Phone number must be a positive integer."
+        | pn >= 0 = PhoneNo pn
+        | otherwise = error "Invalid phone number."
 
--- Function to create a Phone
 toPhone :: Maybe PhoneType -> Maybe CountryCode -> PhoneNo -> Phone
-toPhone pt cc pn = Phone { phoneType=pt, countryCode=cc, phoneNo=pn }
+toPhone pt cc pn = Phone pt cc pn
 
+
+readPhone :: String -> String -> String -> Phone
+readPhone pt cc pn = toPhone (if pt /= "" then Just (read pt) else Nothing)
+                             (toCountryCode $ parseCC cc)
+                             (toPhoneNo $ read pn)
+    where parseCC []           = -1
+          parseCC ('+':cc)     = parseCC cc
+          parseCC ('0':'0':cc) = parseCC cc
+          parseCC cc           = if cc `elem` listOfSomeCountryCodes then read cc else error "Unknown country code."
